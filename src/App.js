b8d475a47197335bb05f7f252e837205e28309b4
Manvir <manvirsinghd@hmail.com>
2023-06-02 10:17:59 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import PdfTrack from './Components/PdfTrack'
+
+export const App = () => {
+
+  return (
+    <div className='container'>
+     <PdfTrack/>
+    </div>
+  )
+}
+
+export default App
